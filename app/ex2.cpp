e7123823fb8b5dcb48d67287e56dcc4b71d7fc70
Gang-Ryung Uh <gangryunguh@gmail.com>
2018-07-02 14:38:12 -0500

    ex1.cpp and ex2.cpp being added

---
+++ app/ex2.cpp
@@ -0,0 +1,43 @@
+// This exercise is to initialize 2D grid from an input file and
+// print this 2D grid. The input file should  first contain two
+// integer values representing the number of rows and columns
+// in the grid.
+// Implement two functions - initGrid and showGrid
+//
+// prepared by Gang-Ryung Uh
+
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+using namespace std;
+
+// Global constants
+const int MAXCOLS = 50;  // Number of columns in each array
+void initGrid(ifstream &, int [][MAXCOLS], int, int);  // Function prototype
+void showGrid(int [][MAXCOLS], int, int); // Function prototype
+
+int main()
+{
+    cout << "Type in the name of the input file: " << flush;
+    string filename;
+    cin >> filename;
+    ifstream fin(filename);
+
+    int rows, cols;
+    int grid[rows][MAXCOLS];
+
+    // initialize the array
+   cout << "Initialize the grid:\n";
+   initGrid(fin, grid, rows, cols);
+   cout << "The contents of initialized grid are:\n";
+   showGrid(grid, rows, cols);
+   return 0;
+}
+
+void initGrid(ifstream &fin, int grid[][MAXCOLS], int r, int c) {
+
+};
+
+void showGrid(int grid[][MAXCOLS], int r, int c) {
+
+};
